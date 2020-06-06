f1f7b8b5fd6f0d00ebf4e44c31ec7a3bf6af09c0
pamelars86 <pamela.rosales@pucp.pe>
2020-06-06 13:58:42 -0300

    sending files to github

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
+// Import the React and ReactDOM libraries
+// import: We want to get code from some other file or dependency
+// React: the variable we want to assign this import to
+// from: 
+// 'react' the name of the dependency or path to the file we are importing
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function getButtonText(){
+    return 'botoncito D'
+}
+
+// Create a react component
+const App = () => {
+  const buttonText = {name: 'pamela'};
+  return (
+  <div>
+    <label className="label" htmlFor="name">Enter name:</label>
+    <input id="name" type="text"/>
+    <button style={{backgroundColor:'blue' , color: 'white'}}>{buttonText.name}</button>
+  </div>);
+};
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
+// component to be rendered
+// 2nd argument: Dom element
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
